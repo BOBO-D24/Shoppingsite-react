17c7972d5d19ac207bbefbd4588718fff16e7b34
Bobo-d <bobodproject@gmail.com>
2024-07-07 23:24:30 +0100

    initial commit

---
+++ shopping-site/src/pages/home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { product } from '../product'
+import ProductCart from '../components/productCart'
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-3xl my-5'>List Products</h1>
+      <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-5'>
+        {product.map((product, key) =>
+            <ProductCart key={key} data={product}/>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Home
